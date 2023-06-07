b1ea98ea6fdd74f57748431cdee37064e92163af
CoulsonP <coulsonp@icloud.com>
2023-06-07 11:13:45 -0500

    finished basic. Just need to style

---
+++ script.js
@@ -0,0 +1,14 @@
+function selectRandomColor() {
+  var colors = ["pink", "orange", "green", "purple", "blue", "white", "yellow", "red", "brown"];
+  var nameInput = document.getElementById("nameInput");
+  var resultDiv = document.getElementById("result");
+
+  var name = nameInput.value;
+  if (name === "") {
+    alert("Please enter your name.");
+    return;
+  }
+
+  var selectedColor = colors[Math.floor(Math.random() * colors.length)];
+  resultDiv.innerHTML = "<p><strong>" + name + "</strong> got the color <span style='color: " + selectedColor + ";'>" + selectedColor + "</span>.</p>";
+}
